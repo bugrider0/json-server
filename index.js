587bb388975998875a9a86c07cdde8b2d9fbe07a
hsn8najafi <hsn8najafi@gmail.com>
2021-12-11 23:37:33 +0330

    Convert json String to a Object

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const express = require("express");
 
 const app = express();
 
+// json File Path
 const jsonFilePath = process.argv[2];
 
-const jsonData = fs.readFileSync(jsonFilePath).toString();
+// Converts String Data to a JavaScript Object
+const data = JSON.parse(fs.readFileSync(jsonFilePath).toString());
 
-console.log(jsonData);
-
+// Home Route
 app.get("/", (req, res) => {
-  res.status(200).json({ name: "hosein" });
+  res.status(200).json(data);
 });
 
+// Event Listner
 const PORT = 3001;
 app.listen(PORT, () =>
   console.log(`Server is Running on : http://127.0.0.1:${PORT}`)
