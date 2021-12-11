2f88f72bad92f7948982690356ed36babeb3a226
hsn8najafi <hsn8najafi@gmail.com>
2021-12-11 23:54:04 +0330

    Router Added

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const fs = require("fs");
-
 const express = require("express");
+
 require("dotenv").config();
 
 const app = express();
 
-// json File Path
-const jsonFilePath = process.argv[2];
-
-// Converts String Data to a JavaScript Object
-const data = JSON.parse(fs.readFileSync(jsonFilePath).toString());
-
 // Home Route
-app.get("/", (req, res) => {
-  res.status(200).json(data);
-});
+app.use(require("./routes/indexRoute"));
 
 // Event Listner
 const PORT = process.env.PORT;
