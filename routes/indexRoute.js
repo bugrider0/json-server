37b09c3c26b45b1a3557e7aefbc844169658356b
hsn8najafi <hsn8najafi@gmail.com>
2021-12-12 00:34:30 +0330

    Controller

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,17 +1,7 @@
-const fs = require("fs");
-
 const { Router } = require("express");
 
 const router = Router();
 
-// json File Path
-const jsonFilePath = process.argv[2];
-
-// Converts String Data to a JavaScript Object
-const data = JSON.parse(fs.readFileSync(jsonFilePath).toString());
-
-router.get("/", (req, res) => {
-  res.status(200).json(data);
-});
+router.get("/", require("../controllers/index"));
 
 module.exports = router;
