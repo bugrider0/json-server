ad9383bc3c47fc82d6d394be83bd1df3d626ab4b
hsn8najafi <hsn8najafi@gmail.com>
2021-12-12 13:43:39 +0330

    By Default Use Internal Json File, if you want use Abother json File use [npm start <PATH>]

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 
 // json File Path
-const jsonFilePath = process.argv[2];
+let jsonFilePath = process.argv[2];
+
+// if Another json File Find Use Instead In Default File
+process.argv[3] ? (jsonFilePath = process.argv[3]) : null;
 
 // Converts String Data to a JavaScript Object
 const data = JSON.parse(fs.readFileSync(jsonFilePath).toString());
